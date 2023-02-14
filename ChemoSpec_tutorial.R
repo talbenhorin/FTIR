d847f8064f4f10967145ab92a0655be3bc15adaa
Tal Ben-Horin PhD <tbenhor@ncsu.edu>
2023-02-14 16:09:18 -0500

    Update

--- ChemoSpec_tutorial.R
+++ ChemoSpec_tutorial.R
@@ -2,5 +2,20 @@ rm(list=ls(all=TRUE))
 set.seed(1234)
 
 library(ChemoSpec)
-files2SpectraObject
+library(ggplot2)
 
+ssp <- files2SpectraObject(
+  gr.crit = c("CF", "NR"),
+  gr.cols = c("red", "blue"),
+  freq.unit = "cm",
+  int.unit = "absorbance intensity",
+  descrip = "River",
+  out.file = "rivers")
+
+data(SrE.IR) 
+
+myt <- expression(bolditalic(Serenoa)~bolditalic(repens)~bold(Extract~IR~Spectra))
+p <- plotSpectra(SrE.IR, which = c(1, 2, 14, 16), yrange = c(0, 1.6),
+                 offset = 0.4, lab.pos = 2200)
+p <- p + ggtitle(myt)
+p # when using ggplot2, you have to "call" the object containing the plot
