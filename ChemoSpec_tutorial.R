9292d1be603e42a40dd3b94e84769ad47558adf8
Tal Ben-Horin PhD <tbenhor@ncsu.edu>
2023-02-15 12:29:15 -0500

    update

--- ChemoSpec_tutorial.R
+++ ChemoSpec_tutorial.R
@@ -15,9 +15,13 @@ ssp <- files2SpectraObject(
 
 data(SrE.IR) 
 
-myt <- expression(bolditalic(Serenoa)~bolditalic(repens)~bold(Extract~IR~Spectra))
-p <- plotSpectra(SrE.IR, which = c(1, 2, 14, 16), yrange = c(0, 1.6),
-                 offset = 0.4, lab.pos = 2200)
-p <- p + ggtitle(myt)
-p # when using ggplot2, you have to "call" the object containing the plot
+ssp <- matrix2SpectraObject(
+  gr.crit = NULL,
+  gr.cols = c("auto"),
+  freq.unit = "cm",
+  int.unit = "absorbance intensity",
+  descrip = "Cape Fear River",
+  in.file = "cf.csv",
+  out.file = "mydata",
+  chk = TRUE)
 
