f6d87731a4841c829c9cf1d90ae429b4cf168a64
Tal Ben-Horin PhD <tbenhor@ncsu.edu>
2023-02-16 16:16:49 -0500

    update

--- ChemoSpec_tutorial.R
+++ ChemoSpec_tutorial.R
@@ -6,22 +6,13 @@ library(ggplot2)
 
 ssp <- files2SpectraObject(
   gr.crit = c("cf", "shr"),
-  gr.cols = c("red", "blue"),
   freq.unit = "cm",
-  int.unit = "absorbance intensity",
-  descrip = "River",
-  out.file = "rivers",
-  ... = t)
+  int.unit = "absorbance",
+  descrip = "Rivers",
+  fileExt = "\\.csv",
+  debug = TRUE,
+  sep = ",", header = TRUE)
 
-data(SrE.IR) 
 
-ssp <- matrix2SpectraObject(
-  gr.crit = NULL,
-  gr.cols = c("auto"),
-  freq.unit = "cm",
-  int.unit = "absorbance intensity",
-  descrip = "Cape Fear River",
-  in.file = "cf.csv",
-  out.file = "mydata",
-  chk = TRUE)
+
 
